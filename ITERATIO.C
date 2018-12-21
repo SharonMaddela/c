b9c1f7f89e3811c6cb880222854e503d1abd5982
SharonMaddela <46022692+SharonMaddela@users.noreply.github.com>
2018-12-21 17:19:03 +0530

    Add files via upload

---
+++ ITERATIO.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i=1;
+clrscr();
+printf("enter the value of n");
+scanf("%d",&n);
+for(i=1;i<=n;i=i+2)
+{
+printf("\n%d",i);
+}
+
+getch();
+}
